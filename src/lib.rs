d5fe6282379dbd4b2fdac146f2fa2af8ec51ea53
kingrongH <kingronghuang@gmail.com>
2022-06-02 23:30:21 +0800

    init: add Invoker and Context and a Typed trait. add test for invoker

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![feature(once_cell)]
+
+mod invoker;
+
+
+trait Typed {
+
+    fn get<I: 'static>(&self) -> Option<&I>;
+
+    fn get_mut<I: 'static>(&mut self) -> Option<&mut I>;
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
